f800300aa67b37a845fad9dd8bfb8689ce899a1e
unknown <vpiyasheva@gmail.com>
2013-07-25 02:42:54 +1200

    Prototype presentation finished

--- js/Main.js
+++ js/Main.js
@@ -5,3 +5,15 @@ function initialize(){
 	example =  1;
 	var anotherExample = "hi";
 }
+
+function addData(){
+	console.log("Adding to database!!!");
+}
+
+function searchData(){
+	console.log("Searching data...");
+}
+
+function help(){
+	console.log("...help. HELP!!!");
+}
