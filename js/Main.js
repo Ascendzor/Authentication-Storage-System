d34ac095a031bfee6e02b45c697a6e95a6ea3acf
tmare <bob.craker@hotmail.com>
2013-07-25 04:01:29 -0700

    polished buttons to highlight when hovered over and darken when clicked

--- js/Main.js
+++ js/Main.js
@@ -1,27 +1,54 @@
-var example;
-
+//called as soon as <body> finished loading
 function initialize(){
-	console.log("initialize method called");
-	example =  1;
-	var anotherExample = "hi";
 }
 
+//called when the mouse overlaps a menu item
 function highlight(id){
+	if(document.getElementById(id).className == "selectedMenuItem"){
+		return;
+	}
 	document.getElementById(id).className = "highlightedMenuItem";
 }
 
+//called when the mouse stops overlapping a menu item
 function lowlight(id){
+	if(document.getElementById(id).className == "selectedMenuItem"){
+		return;
+	}
 	document.getElementById(id).className = "menuItem";
 }
 
+//called when a menu item is clicked
+function menuItemClicked(id){
+	var selectedMenus = document.getElementsByClassName("selectedMenuItem");
+	for(var i=0; i<selectedMenus.length; i++){
+		document.getElementById(selectedMenus[i].id).className = "menuItem";
+	}
+	document.getElementById(id).className = "selectedMenuItem";
+	
+	if(id == "addData"){
+		addData();
+	}else if(id == "searchData"){
+		searchData();
+	}else if(id == "help"){
+		help();
+	}
+}
+
+//called when the addData menu item is clicked
 function addData(){
-	console.log("Adding to database!!!");
+	var content = "<p>space for adding account details to the database</p>";
+	document.getElementById("content").innerHTML = content;
 }
 
+//called when the searchData menu item is clicked
 function searchData(){
-	console.log("Searching data...");
+	var content = "<p>space for searching for account details on the database</p>";
+	document.getElementById("content").innerHTML = content;
 }
 
+//called when the help menu item is clicked
 function help(){
-	console.log("...help. HELP!!!");
+	var content = "<p>space for advice and demonstrations on how to use this program</p>";
+	document.getElementById("content").innerHTML = content;
 }
