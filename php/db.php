c52a5a5f39eccb52990b7dc7192e1ec70efdf42b
unknown <abc.zacbil@gmail.com>
2013-07-26 20:17:09 +1200

    php folder added

---
+++ php/db.php
@@ -0,0 +1,9 @@
+<?php
+   $sql_server = "webcloud45.au.syrahost.com";
+   $db_username = "optimald";
+   $db_password = "Foodroptable01";
+   $db_name = "optimald_testdatabase";
+
+   mysql_connect($sql_server, $db_username, $db_password);
+   mysql_select_db($db_name);
+?>
